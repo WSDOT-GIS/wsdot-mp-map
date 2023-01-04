2231b3e09cb0c5d031d99a6432252c9a268686ea
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2023-01-04 13:30:08 -0800

    Added leaflet map

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,15 @@
+import { map, tileLayer, LatLngBounds } from "leaflet";
 import 'leaflet/dist/leaflet.css'
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const waExtent = new LatLngBounds([
+  [45.54, -116.91],
+  [49.05, -124.79]
+]);
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const theMap = map("map").fitBounds(waExtent);
+
+tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(theMap);
