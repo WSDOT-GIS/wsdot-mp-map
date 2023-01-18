c27fcde22a4e405892e731b618c5334093a8a494
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2023-01-18 15:31:34 -0800

    chore: :construction: Work in progress on feature service query

--- src/arcgis/featureServiceQuery.ts
+++ src/arcgis/featureServiceQuery.ts
@@ -1,62 +1,41 @@
-import { request } from "@esri/arcgis-rest-request";
+import { IRequestOptions, request } from "@esri/arcgis-rest-request";
 import type {
   AttributeValue,
   FeatureServiceQueryResponse,
-  LayerDef,
   QueryResponseLayer,
 } from "./typesAndInterfaces";
 
 export const defaultUrl =
   "https://data.wsdot.wa.gov/arcgis/rest/services/DataLibrary/DataLibrary/FeatureServer/";
 
-type FieldName = "LDNM" | "JURLBL" | "CityName"; // cspell:disable-line
-
-/**
- * Mapping of layer IDs to output field names.
- */
-const fields = new Map<number, FieldName[]>([
-  // cspell:disable
-  [2, ["LDNM"]],
-  [3, ["JURLBL"]],
-  [5, ["CityName"]],
-  // cspell:enable
-]);
-
-const selectAllStatement = "1=1";
-
-const layerDefs: LayerDef<typeof selectAllStatement, FieldName>[] = [...fields.entries()].map(
-  ([layerId, outFields]) => {
-    return {
-      layerId,
-      where: selectAllStatement,
-      outFields,
-    };
-  }
-);
+const layerDefs =
+'[{"layerId":2,"where":"LDNM is not NULL","outFields":"LDNM"},{"layerId":3,"where":"JURLBL is not NULL","outFields":"JURLBL"},{"layerId":5,"where":"CityName is not NULL","outFields":"CityName"}]';
 
 /**
  * Query a feature service for features that intersect with
  * the given point.
  * @param xy - WGS 86 XY coordinates
  * @param featureServiceUrl - Feature service URL
- * @returns 
+ * @returns
  */
 export async function query(
   xy: [number, number],
   featureServiceUrl: string = defaultUrl
 ) {
   const queryUrl = new URL("query", featureServiceUrl);
-  console.debug(`query url: ${queryUrl}`, queryUrl)
-  const response = (await request(queryUrl.toString(), {
+  const queryParams: IRequestOptions = {
     params: {
       geometry: xy.join(","),
+      geometryType: "esriGeometryPoint",
       inSR: 4326,
-      spatialRel: "esriSpatialRelWithin",
-      layerDefs,
+      spatialRel: "esriSpatialRelIntersects",
+      layerDefs: layerDefs,
       returnGeometry: false,
       f: "json",
     },
-  })) as FeatureServiceQueryResponse;
+  };
+  console.debug("query params", queryParams)
+  const response = (await request(queryUrl.toString(), queryParams)) as FeatureServiceQueryResponse;
 
   return response;
 }
@@ -78,9 +57,15 @@ function* enumerateFieldAliases(layer: QueryResponseLayer) {
       const { name } = field;
       let { alias } = field;
       alias ??= name;
-      yield [name, alias] as [FieldName, typeof alias];
+      yield [name, alias] as [string, string];
     }
   }
+}
+
+function createRegex(...items: string[]) {
+  items = items.map((i) => `(?:${i})`);
+  const pattern = `^(${items.join("|")})$`;
+  return new RegExp(pattern, "i");
 }
 
 /**
@@ -100,13 +85,22 @@ function* enumerateFieldAliases(layer: QueryResponseLayer) {
  * }
  */
 export function* enumerateQueryResponseAttributes(
-  response: FeatureServiceQueryResponse
+  response: FeatureServiceQueryResponse,
+  fieldsToOmit: string[] = ["OBJECTID"]
 ) {
+  const fieldsToOmitRegex =
+    fieldsToOmit && fieldsToOmit.length ? createRegex(...fieldsToOmit) : null;
   for (const layer of response.layers) {
     for (const feature of layer.features) {
-      const aliasMap = new Map<string, string>(enumerateFieldAliases(layer));
+      const aliasMap = new Map<string, string>([
+        ...enumerateFieldAliases(layer),
+      ]);
       for (const name in feature.attributes as Record<string, AttributeValue>) {
         if (Object.prototype.hasOwnProperty.call(feature.attributes, name)) {
+          // Skip unwanted fields.
+          if (fieldsToOmitRegex && fieldsToOmitRegex.test(name)) {
+            continue;
+          }
           const value = feature.attributes[name] as AttributeValue;
           // The output of aliasMap.get should always be
           // string rather than undefined, but just in case,
