e192198837bfecadf0a29fa75f5af6a54eac2666
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2023-02-06 16:55:25 -0800

    Customized popup (#53)

---
+++ tests/Geohack.test.ts
@@ -0,0 +1,10 @@
+import { expect, test } from "vitest";
+import { createGeoHackUrl } from "../src/geohack";
+
+test("create geohack URL", () => {
+    const [lat, lng] = [45.6448,-122.6617];
+    const url = createGeoHackUrl([lat, lng]);
+    // Create regex to account for URL encoding of ";" character.
+    const expectedRe = new RegExp(`https://geohack.toolforge.org/geohack.php\\?params=${lat}(;|(%3B))${lng}`);
+    expect(url.href).toMatch(expectedRe);
+})
