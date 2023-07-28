3e3086831beb4891b8b6c686a7d19e5466033669
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2023-07-27 17:35:59 -0700

    test: :white_check_mark: Fixed test scripts

--- tests/Geohack.test.ts
+++ tests/Geohack.test.ts
@@ -1,10 +1,12 @@
 import { expect, test } from "vitest";
-import { createGeoHackUrl } from "../src/geohack";
+import { createGeoHackUrl } from "../src/common/geohack";
 
 test("create geohack URL", () => {
-    const [lat, lng] = [45.6448,-122.6617];
-    const url = createGeoHackUrl([lat, lng]);
-    // Create regex to account for URL encoding of ";" character.
-    const expectedRe = new RegExp(`https://geohack.toolforge.org/geohack.php\\?params=${lat}(;|(%3B))${lng}`);
-    expect(url.href).toMatch(expectedRe);
-})
+  const [lat, lng] = [45.6448, -122.6617];
+  const url = createGeoHackUrl([lat, lng]);
+  // Create regex to account for URL encoding of ";" character.
+  const expectedRe = new RegExp(
+    String.raw`https://geohack.toolforge.org/geohack.php\?params=${lat}(;|(%3B))${lng}`
+  );
+  expect(url.href).toMatch(expectedRe);
+});
