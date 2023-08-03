61e512280119ca08b6a54494bc490430401015e0
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2023-08-03 11:50:44 -0700

    Add user input (#156)
    * feat: :sparkles: Added map loading indicator
    * feat: :sparkles: Added SRMP input form
        Added a form that allows the user to enter a state route and milepost
    * build: :arrow_up: Upgraded dependencies

--- src/elc.ts
+++ src/elc.ts
@@ -1,15 +1,17 @@
 import Graphic from "@arcgis/core/Graphic";
 import Point from "@arcgis/core/geometry/Point";
+import type FeatureLayer from "@arcgis/core/layers/FeatureLayer";
 import type MapView from "@arcgis/core/views/MapView";
 import {
   RouteLocator,
   type IFindNearestRouteLocationParameters,
   type IRouteLocation,
-  type RouteLocation,
+  RouteLocation,
 } from "wsdot-elc";
 import { enumerateQueryResponseAttributes, query } from "./common";
-import { AttributesObject } from "./types";
-import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
+import type { AttributesObject } from "./types";
+import type { RouteEventObject } from "./widgets/SrmpInputForm";
+import NotImplementedError from "./common/NotImplementedError";
 
 type ElcSetupOptions = Pick<
   IFindNearestRouteLocationParameters,
@@ -143,6 +145,49 @@ function isPoint(
 }
 
 /**
+ * Adds a graphic to the map of the ELC result.
+ * @param elcResponse - Response from call to ELC
+ * @param view - Map view
+ * @param milepostLayer - Milepost feature layer to which the graphic will be added
+ * @param mapPoint - Point the user clicked (if applicable)
+ * @returns - The added graphic, or null if the graphic couldn't be added
+ * (e.g., if user clicked an area outside of the search radius of a route.)
+ */
+async function addElcGraphic(
+  elcResponse: RouteLocation[],
+  view: MapView,
+  milepostLayer: FeatureLayer,
+  mapPoint?: Point
+) {
+  const [routeLocation] = elcResponse;
+
+  // Show a popup and exit if the RouteGeometry is not a point.
+  if (!isPoint(routeLocation.RouteGeometry)) {
+    const message = "Unexpected output from ELC.";
+    /* @__PURE__ */ console.warn(message, elcResponse);
+    view
+      .openPopup({
+        content: message,
+        location: mapPoint,
+      })
+      .catch((reason) =>
+        /* @__PURE__ */ console.error(`Error opening popup: ${reason}`)
+      );
+    return null;
+  }
+
+  const graphic = await routeLocationToGraphic(routeLocation);
+  // Add location to the layer.
+  milepostLayer
+    .applyEdits({
+      addFeatures: [graphic],
+    })
+    .catch((reason) => console.error(reason));
+
+  return graphic;
+}
+
+/**
  * Makes a call to the ELC and adds a point graphic to the {@link milepostLayer}.
  * @param view
  * @param milepostLayer
@@ -190,28 +235,47 @@ export async function callElc(
     return null;
   }
 
-  const [routeLocation] = elcResponse;
+  const graphic = await addElcGraphic(
+    elcResponse,
+    view,
+    milepostLayer,
+    mapPoint
+  );
 
-  // Show a popup and exit if the RouteGeometry is not a point.
-  if (!isPoint(routeLocation.RouteGeometry)) {
-    const message = "Unexpected output from ELC.";
-    /* @__PURE__ */ console.warn(message, elcResponse);
-    view
-      .openPopup({
-        content: message,
-        location: mapPoint,
-      })
-      .catch((reason) => console.error(reason));
-    return null;
+  return graphic;
+}
+
+/**
+ *
+ * @param e - The event from the user submitting a milepost via the form
+ * @param view - The map view
+ * @param milepostLayer - The milepost layer where the result graphic will be added.
+ * @param mapPoint - The point where
+ */
+export async function callElcFromForm(
+  e: RouteEventObject,
+  view: MapView,
+  milepostLayer: FeatureLayer
+) {
+  const rl = new RouteLocator();
+  /* @__PURE__ */ console.debug(
+    `${callElcFromForm.name}: RouteEventObject type: ${e.type ?? "null"}`
+  );
+  if (e.type != null) {
+    throw new NotImplementedError(
+      "non-mainline types have not been implemented."
+    );
   }
+  const location = new RouteLocation({
+    Route: e.route,
+    Srmp: e.mp,
+  });
+  const elcResponse = await rl.findRouteLocations({
+    locations: [location],
+    outSR: view.spatialReference.wkid,
+    referenceDate: new Date(),
+  });
 
-  const graphic = await routeLocationToGraphic(routeLocation);
-  // Add location to the layer.
-  milepostLayer
-    .applyEdits({
-      addFeatures: [graphic],
-    })
-    .catch((reason) => console.error(reason));
-
+  const graphic = await addElcGraphic(elcResponse, view, milepostLayer);
   return graphic;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,172 +1,201 @@
-import type View from "@arcgis/core/views/View";
+import { callElcFromForm } from "./elc";
 
 import("./index.css");
 
-/**
- * Sets up the loading indicator for a map view.
- * @param view
- * @returns - The handle for watching the view's updating property.
- */
-function setupViewLoadingIndicator(view: View) {
-  const viewProgress = document.createElement("progress");
-  viewProgress.textContent = "Updating map...";
-  // Add the map loading indicator.
-  view.ui.add(viewProgress, "bottom-trailing");
-  // Make the view loading indicator only show up when the map is updating.
-  return view.watch(
-    "updating",
-    (updating) => (viewProgress.hidden = !updating)
+// Using an async self-executing function because
+// top-level awaits aren't allowed.
+(async () => {
+  // Asynchronously import modules. This helps build generate smaller chunks.
+  const [
+    { default: EsriMap },
+    { default: config },
+    { default: MapView },
+    { default: ScaleBar },
+    { default: Home },
+    { createMilepostLayer },
+    { waExtent },
+    { callElc },
+    { setupWidgets },
+    { setupSearch },
+    { isGraphicHit },
+  ] = await Promise.all([
+    import("@arcgis/core/Map"),
+    import("@arcgis/core/config"),
+    import("@arcgis/core/views/MapView"),
+    import("@arcgis/core/widgets/ScaleBar"),
+    import("@arcgis/core/widgets/Home"),
+    import("./MilepostLayer"),
+    import("./WAExtent"),
+    import("./elc"),
+    import("./widgets/expandGroups"),
+    import("./widgets/setupSearch"),
+    import("./types"),
+  ]);
+
+  config.applicationName = "WSDOT Mileposts";
+  config.log.level = import.meta.env.DEV ? "info" : "error";
+  const { request } = config;
+  // This app only uses publicly available map services,
+  // so we don't need to use identity.
+  request.useIdentity = false;
+  // Initialize httpDomains array if it does not already have a value.
+  if (!request.httpsDomains) {
+    request.httpsDomains = [];
+  }
+  request.httpsDomains.push("wsdot.wa.gov", "data.wsdot.wa.gov");
+
+  const milepostLayer = await createMilepostLayer(waExtent.spatialReference);
+
+  const map = new EsriMap({
+    basemap: "hybrid",
+    layers: [milepostLayer],
+  });
+
+  const view = new MapView({
+    container: "viewDiv",
+    map,
+    constraints: {
+      geometry: waExtent,
+      minZoom: 7,
+    },
+    extent: waExtent,
+    popupEnabled: false,
+  });
+
+  // Add the loading indicator widget to the map.
+  import("./widgets/LoadingIndicator").then(
+    ({ setupViewLoadingIndicator }) => setupViewLoadingIndicator(view),
+    (reason) =>
+      /* @__PURE__ */ console.error(
+        `Failed to add loading indicator: ${reason}`
+      )
   );
-}
 
-Promise.all([
-  import("@arcgis/core/Map"),
-  import("@arcgis/core/config"),
-  import("@arcgis/core/views/MapView"),
-  import("@arcgis/core/widgets/ScaleBar"),
-  import("@arcgis/core/widgets/Home"),
-  import("./MilepostLayer"),
-  import("./WAExtent"),
-  import("./elc"),
-  import("./widgets/expandGroups"),
-  import("./widgets/setupSearch"),
-  import("./types"),
-])
-  .then(
-    async ([
-      { default: EsriMap },
-      { default: config },
-      { default: MapView },
-      { default: ScaleBar },
-      { default: Home },
-      { createMilepostLayer },
-      { waExtent },
-      { callElc },
-      { setupWidgets },
-      { setupSearch },
-      { isGraphicHit },
-    ]) => {
-      config.applicationName = "WSDOT Mileposts";
-      config.log.level = import.meta.env.DEV ? "info" : "error";
-      const { request } = config;
-      // This app only uses publicly available map services,
-      // so we don't need to use identity.
-      request.useIdentity = false;
-      // Initialize httpDomains array if it does not already have a value.
-      if (!request.httpsDomains) {
-        request.httpsDomains = [];
-      }
-      request.httpsDomains.push("wsdot.wa.gov", "data.wsdot.wa.gov");
+  const sb = new ScaleBar({
+    unit: "dual",
+    view,
+  });
+  view.ui.add(sb, "bottom-leading");
 
-      const milepostLayer = await createMilepostLayer(
-        waExtent.spatialReference
-      );
+  view.popup.defaultPopupTemplateEnabled = true;
 
-      const map = new EsriMap({
-        basemap: "hybrid",
-        layers: [milepostLayer],
-      });
+  const search = setupSearch(view);
+  search.view.ui.add(search, {
+    index: 0,
+    position: "top-trailing",
+  });
 
-      const view = new MapView({
-        container: "viewDiv",
-        map,
-        constraints: {
-          geometry: waExtent,
-          minZoom: 7,
-        },
-        extent: waExtent,
-        popupEnabled: false,
-      });
+  setupWidgets(view, "top-trailing", {
+    group: "top-trailing",
+  });
 
-      setupViewLoadingIndicator(view);
+  const home = new Home({
+    view,
+  });
 
-      const sb = new ScaleBar({
-        unit: "dual",
-        view,
-      });
-      view.ui.add(sb, "bottom-leading");
+  view.ui.add(home, "top-trailing");
 
-      view.popup.defaultPopupTemplateEnabled = true;
-
-      const search = setupSearch(view);
-      search.view.ui.add(search, {
+  import("./widgets/SrmpInputForm").then(
+    ({ createSrmpInputForm, isRouteInputEvent }) => {
+      const form = createSrmpInputForm(view.ui, {
         index: 0,
-        position: "top-trailing",
+        position: "top-leading",
       });
-
-      setupWidgets(view, "top-trailing", {
-        group: "top-trailing",
-      });
-
-      const home = new Home({
-        view,
-      });
-
-      view.ui.add(home, "top-trailing");
-
-      const defaultSearchRadius = 3000;
-      // eslint-disable-next-line @typescript-eslint/no-misused-promises
-      view.on("click", async (event) => {
-        // Test to see if the clicked point intersects any of the milepost graphics.
-        const hitTestResult = await view.hitTest(event, {
-          include: milepostLayer,
-        });
-
-        // If the user clicked on a milepost graphic, open a popup for the graphic
-        // and exit.
-        if (hitTestResult.results.length > 0) {
-          // Extract the features from the hit test results object's "results" property.
-          const features = hitTestResult.results
-            // Filter out any that are not of type "graphic".
-            // Since we are only testing against a FeatureLayer,
-            // all of them should be "graphic"
-            .filter(isGraphicHit)
-            .map((viewHit) => viewHit.graphic);
-          view
-            .openPopup({
-              location: event.mapPoint,
-              features,
-            })
-            .then(
-              () => {},
-              (reason) => {
-                /* @__PURE__ */ console.error(reason);
-              }
+      form.addEventListener(
+        "srmp-input",
+        (e) => {
+          if (!isRouteInputEvent(e)) {
+            /* @__PURE__ */ console.warn(
+              "Input is not in expected format",
+              e instanceof CustomEvent ? e.detail : e
             );
-          return;
-        }
+            return;
+          }
+          /* @__PURE__ */ console.debug("User inputted a milepost", e.detail);
 
-        const graphic = await callElc(view, milepostLayer, event.mapPoint, {
-          searchRadius: defaultSearchRadius,
-          useCors: true,
-        });
-
-        if (graphic == null) {
-          return;
-        }
-
-        view
-          .openPopup({
-            features: [graphic],
-            fetchFeatures: true,
-            shouldFocus: true,
-            updateLocationEnabled: true,
-          })
-          .then(
-            () => {},
+          callElcFromForm(e.detail, view, milepostLayer).then(
+            (elcGraphic) => {
+              if (!elcGraphic) {
+                /* @__PURE__ */ console.log(
+                  "Returned graphic from user input",
+                  elcGraphic
+                );
+              } else {
+                /* @__PURE__ */ console.warn(
+                  "User input resulted in null graphic."
+                );
+              }
+            },
             (reason) => {
-              /* @__PURE__ */ console.error(reason);
+              /* @__PURE__ */ console.error(callElcFromForm.name, reason);
             }
           );
-      });
-    }
-  )
-  .then(
-    () => {},
-    (reason) => {
-      /* @__PURE__ */ console.error(
-        `An error was encountered in "main".`,
-        reason
+        },
+        {
+          passive: true,
+        }
       );
+    },
+    (reason) => {
+      console.error("SrmpInputForm module import", reason);
     }
   );
+
+  const defaultSearchRadius = 3000;
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  view.on("click", async (event) => {
+    // Test to see if the clicked point intersects any of the milepost graphics.
+    const hitTestResult = await view.hitTest(event, {
+      include: milepostLayer,
+    });
+
+    // If the user clicked on a milepost graphic, open a popup for the graphic
+    // and exit.
+    if (hitTestResult.results.length > 0) {
+      // Extract the features from the hit test results object's "results" property.
+      const features = hitTestResult.results
+        // Filter out any that are not of type "graphic".
+        // Since we are only testing against a FeatureLayer,
+        // all of them should be "graphic"
+        .filter(isGraphicHit)
+        .map((viewHit) => viewHit.graphic);
+      view
+        .openPopup({
+          location: event.mapPoint,
+          features,
+        })
+        .then(
+          () => {},
+          (reason) => {
+            /* @__PURE__ */ console.error(reason);
+          }
+        );
+      return;
+    }
+
+    const graphic = await callElc(view, milepostLayer, event.mapPoint, {
+      searchRadius: defaultSearchRadius,
+      useCors: true,
+    });
+
+    if (graphic == null) {
+      return;
+    }
+
+    view
+      .openPopup({
+        features: [graphic],
+        fetchFeatures: true,
+        shouldFocus: true,
+        updateLocationEnabled: true,
+      })
+      .then(
+        () => {},
+        (reason) => {
+          /* @__PURE__ */ console.error(reason);
+        }
+      );
+  });
+})().catch((reason) => {
+  console.error(reason);
+});
