ae5baadcfabdf66700974077dee39e295b5a5db3
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2023-10-05 15:33:34 -0700

    refactor: :rotating_light: Modified clear button creation to resolve lint errors

--- src/widgets/ClearButton.ts
+++ src/widgets/ClearButton.ts
@@ -1,20 +1,39 @@
-import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
+import type FeatureLayer from "@arcgis/core/layers/FeatureLayer";
 
-interface ClearButtonOptions {
+/**
+ * clear button creation options
+ */
+export interface ClearButtonOptions {
+  /**
+   * The layer that will be controlled by the button
+   */
   layer: FeatureLayer;
 }
 
+/**
+ * Creates a button that will clear all graphics on the layer associated with it.
+ * @param options - button creation options
+ * @returns A button that will clear the specified graphics when cleared.
+ */
 export function createClearButton(options: ClearButtonOptions) {
   const button = document.createElement("button");
   button.innerText = "Clear";
+  const { layer } = options;
 
-  button.addEventListener("click", async () => {
-    const { layer } = options;
-    const features = await layer.queryFeatures();
-    await layer.applyEdits({
-      deleteFeatures: features.features,
-    });
-  });
+  function clearFeatures(this: HTMLButtonElement): void {
+    layer
+      .queryFeatures()
+      .then((features) =>
+        layer.applyEdits({
+          deleteFeatures: features.features,
+        })
+      )
+      .then((editsResult) => {
+        /* @__PURE__ */ console.debug("edits result", editsResult);
+      })
+      .catch(console.error);
+  }
 
+  button.addEventListener("click", clearFeatures);
   return button;
 }
