0c62f3ab7f91f533c3347047d1f460ecf7133204
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2024-04-01 14:41:39 -0700

    chore: :hammer: Added get-arcgis-core-files

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 /// <reference types="vite/client" />
 /// <reference types="vitest" />
+
+import type { ExternalOption } from "rollup";
 import browserslistToEsbuild from "browserslist-to-esbuild";
 import { defineConfig } from "vite";
 import checker from "vite-plugin-checker";
@@ -9,68 +11,98 @@ import checker from "vite-plugin-checker";
 //   "node_modules/@esri/calcite-components/dist/calcite/assets/";
 // const calciteAssetsSourceDir = "public/";
 
-export default defineConfig({
-  appType: "spa",
-  base: "/data/tools/LocateMP",
-  resolve: {
-    extensions: [".ts", ".js", ".css", ".json"],
-  },
-  build: {
-    // This sets the target based on the `browserslist` file.
-    target: browserslistToEsbuild(),
-    rollupOptions: {
-      input: {
-        main: "./index.html",
+type ExternalFunction = Exclude<ExternalOption, string | RegExp | unknown[]>;
+
+const external: ExternalFunction = (id, parentId, isResolved) => {
+  console.log("external function", { id, parentId, isResolved });
+  // TODO: implement logic to skip unwanted @arcgis/core modules.
+  return false;
+};
+
+export default defineConfig((env) => {
+  return {
+    appType: "spa",
+    base: "/data/tools/LocateMP",
+    resolve: {
+      extensions: [".ts", ".js", ".css", ".json"],
+    },
+    build: {
+      // This sets the target based on the `browserslist` file.
+      target: browserslistToEsbuild(),
+      rollupOptions: {
+        input: {
+          main: "./index.html",
+        },
+        external,
+        // external: [
+        //   /BingMaps/i,
+        //   /GeoJSON/i,
+        //   /GeoRSS/i,
+        //   /integratedMeshLayer/i,
+        //   /KML/i,
+        //   /KnowledgeGraph/i,
+        //   /OGCFeature/i,
+        //   /OpenStreetMap/i,
+        //   /Oriented/i,
+        //   /PointCloud/i,
+        //   /RouteLayer/i,
+        //   /sphere/i,
+        //   /StreamLayer/i,
+        //   /Video/i,
+        //   /Voxel/i,
+        //   /WFS/i,
+        //   /WMT?SLayer/i,
+        // ],
+      },
+      sourcemap: command === "serve",
+    },
+    plugins: [
+      checker({
+        typescript: true,
+        eslint: {
+          lintCommand: "eslint .",
+          useFlatConfig: true,
+        },
+      }),
+      // copy({
+      //   targets: [
+      //     {
+      //       src: calciteAssetsDestDir,
+      //       dest: calciteAssetsSourceDir,
+      //     },
+      //   ],
+      // }),
+    ],
+    test: {
+      name: "LocateMP",
+      environment: "jsdom",
+      reporters: ["default", "junit"],
+      outputFile: {
+        junit: "./test-results/test-results.xml",
+      },
+      coverage: {
+        enabled: true,
+        extension: [
+          // '.js',
+          // '.cjs',
+          // '.mjs',
+          // ".jsx",
+          ".ts",
+          ".tsx",
+          // ".vue",
+          // ".svelte",
+        ],
+        reporter: [
+          // Defaults
+          "text",
+          "html",
+          "clover",
+          "json",
+          // Added
+          "lcov",
+          "cobertura",
+        ],
       },
     },
-    sourcemap: command === "serve",
-  },
-  plugins: [
-    checker({
-      typescript: true,
-      eslint: {
-        lintCommand: "eslint .",
-        useFlatConfig: true,
-      },
-    }),
-    // copy({
-    //   targets: [
-    //     {
-    //       src: calciteAssetsDestDir,
-    //       dest: calciteAssetsSourceDir,
-    //     },
-    //   ],
-    // }),
-  ],
-  test: {
-    name: "LocateMP",
-    environment: "jsdom",
-    reporters: ["default", "junit"],
-    outputFile: {
-      junit: "./test-results/test-results.xml",
-    },
-    coverage: {
-      enabled: true,
-      extension: [
-        // '.js',
-        // '.cjs',
-        // '.mjs',
-        // ".jsx",
-        ".ts",
-        ".tsx",
-        // ".vue",
-        // ".svelte",
-      ],
-      reporter: [
-        // Defaults
-        "text",
-        "html",
-        "clover",
-        "json",
-        // Added
-        "lcov",
-        "cobertura",
-      ],
-    },
-  },
+  };
 });
