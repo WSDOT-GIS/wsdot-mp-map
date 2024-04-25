52375a72781d170e48de4fb046d8076e331a2ce1
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2024-04-25 15:09:59 -0700

    test: :white_check_mark: Added ELC URL test

--- tests/url.test.ts
+++ tests/url.test.ts
@@ -24,19 +24,26 @@ describe("URL search parameters", () => {
     const actual = getElcParamsFromUrl(url);
     expect(actual).toEqual(expected);
   });
-  // test("AR specified", () => {
-  //   const p = {
-  //     SR: "503",
-  //     MP: "35.23",
-  //     RT: "SP",
-  //   };
-  //   const expected: ExpectedType = {
-  //     Route: "503",
-  //     Srmp: 35.23,
-  //     Back: false,
-  //     Decrease: false,
-  //     ReferenceDate: expect.any(Date),
-  //     ResponseDate: expect.any(Date),
-  //   };
-  // });
+  test("AR specified", () => {
+    const p = new URLSearchParams([
+      ["SR", "503"],
+      ["MP", "35.23"],
+      ["RT", "SP"],
+    ] as const);
+
+    const expected: ExpectedType = {
+      Route: "503SP",
+      Srmp: 35.23,
+      Back: false,
+      Decrease: false,
+      ReferenceDate: expect.any(Date) as Date,
+      ResponseDate: expect.any(Date) as Date,
+    };
+
+    const url = new URL(`${urlRoot}?${p.toString()}`);
+
+    const actual = getElcParamsFromUrl(url);
+
+    expect(actual).toEqual(expected);
+  });
 });
