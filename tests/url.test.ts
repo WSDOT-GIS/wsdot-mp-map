4da3a34529139b789f3d5f75b84a22b83235de0e
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2024-05-08 12:22:58 -0700

    test: :white_check_mark: Added test for url with RRT but no RRQ

--- tests/url.test.ts
+++ tests/url.test.ts
@@ -46,4 +46,60 @@ describe("URL search parameters", () => {
 
     expect(actual).toEqual(expected);
   });
+
+  /*
+  Works
+
+    SR=20&mp=52&RT=SP&RRQ=ANACRT
+
+  Doesn't work
+
+      SR=20&mp=52&RT=SP goes to 020 @ MP 52, not the Spur (Anacortes)
+  */
+  // Test URL creation when an Route Type is specified but not an RRQ.
+  test("Route Type specified", () => {
+    const p = new URLSearchParams([
+      ["SR", "20"],
+      ["MP", "52"],
+      ["RT", "SP"],
+    ] as const);
+
+    const url = new URL(`${urlRoot}?${p.toString()}`);
+
+    const actual = getElcParamsFromUrl(url);
+    const expected = {
+      Route: "020SP",
+      Srmp: 52,
+      Back: false,
+      Decrease: false,
+      ReferenceDate: expect.any(Date) as Date,
+      ResponseDate: expect.any(Date) as Date,
+    };
+
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe("RRT w/o RRQ", () => {
+  test("getElcParamsFromUrl", async () => {
+    const p = new URLSearchParams([
+      ["SR", "20"],
+      ["MP", "52"],
+      ["RT", "SP"],
+    ] as const);
+    const url = new URL(`${urlRoot}?${p.toString()}`);
+    const elcParams = getElcParamsFromUrl(url);
+    expect(elcParams).not.toBeNull();
+    if (elcParams) {
+      const { findRouteLocations } = await import("../src/elc");
+      const result = await findRouteLocations({
+        locations: [elcParams],
+        outSR: 3857,
+      });
+      console.log("result", result);
+      expect(result).toHaveLength(1);
+      const errorResults = result.filter((r) => r instanceof Error);
+      expect(errorResults).toHaveLength(1);
+    }
+  });
 });
