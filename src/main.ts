e671721f6966a065fd8740f6995464588ccbfdfc
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2024-05-21 12:28:32 -0700

    refactor: :goal_net: Added error handling when WSDOT Logo fails to load.
    refactor: :bulb: Added JSDoc and other comments

--- src/main.ts
+++ src/main.ts
@@ -7,26 +7,42 @@ import "@fontsource/lato";
 
 import("@wsdot/web-styles/css/wsdot-colors.css");
 
+/**
+ * Asynchronously adds the WSDOT logo SVG to the HTML document.
+ * @returns A Promise that resolves with the WSDOT logo element.
+ * @throws {Error} If the heading element is not found.
+ */
 async function addWsdotLogo() {
+  // Import raw SVG markup from SVG file.
   const { default: svg } = await import(
     "@wsdot/web-styles/images/wsdot-logo/wsdot-logo-black.svg?raw"
   );
+  // Parse the markup into a DOM element.
   const dp = new DOMParser();
   const wsdotLogo = dp.parseFromString(svg, "image/svg+xml").documentElement;
 
+  // Add an id attribute.
   wsdotLogo.id = "wsdot-logo";
 
-  /* __PURE__ */ console.debug("wsdot logo", wsdotLogo);
+  // Add the logo to the heading element.
+  // Throw an error if the heading element cannot found.
   const headingSelector = "h2";
   const headingElement = document.body.querySelector(headingSelector);
   if (!headingElement) {
     throw new Error("Heading element not found");
   }
+  // Prepend the logo to the heading element.
   headingElement.prepend(wsdotLogo);
+  // Return the logo element.
   return wsdotLogo;
 }
 
-void addWsdotLogo();
+addWsdotLogo().catch((reason: unknown) => {
+  if (import.meta.env.DEV) {
+    emitErrorEvent(reason);
+  }
+  console.error("Failed to add WSDOT logo.", reason);
+});
 
 window.addEventListener("elc-error", (event) => {
   /* __PURE__ */ console.group("elc-error event listener");
