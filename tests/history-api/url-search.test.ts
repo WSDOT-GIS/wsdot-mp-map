5c5e2078d460b71641f83c50fa3871a1ca31f951
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2024-06-06 17:04:32 -0700

    refactor: Added new URL manipulation functions

---
+++ tests/history-api/url-search.test.ts
@@ -0,0 +1,25 @@
+import { moveUrlSearchToHash } from "../../src/history-api/url-search";
+import { test, expect, describe } from "vitest";
+
+describe("moveUrlSearchToHash", () => {
+  test("moves the URL search parameters to the URL hash", () => {
+    const x = -122.90913367530709;
+    const y = 46.14502682037163;
+    const zoom = 15;
+    const bearing = 0;
+    const pitch = 0;
+
+    const sr = "004";
+    const mp = 61.65;
+    const urlRoot = "http://example.com/data/tools/Locatemp/";
+    const qs = `MP=${mp}&SR=${sr}`;
+    const hash = `${zoom}/${y}/${x}/${bearing}/${pitch}`;
+    const url = new URL(`?${qs}#${hash}`, urlRoot);
+
+    const updatedUrl = moveUrlSearchToHash(url);
+
+    const expectedUrl = new URL(`#${hash}?${qs}`, urlRoot);
+
+    expect(updatedUrl.toString()).toBe(expectedUrl.toString());
+  });
+});
