688db72165a3a0b7519cc7aeb4241b0b15067d49
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2024-06-10 16:57:28 -0700

    chore: :wrench: Modified lefthood.yml to use tools' caching options

--- tests/hash.test.ts
+++ tests/hash.test.ts
@@ -2,25 +2,6 @@ import { parseMapPositionHash } from "../src/history-api";
 import { test, expect, describe } from "vitest";
 
 describe("parseMapPositionHash", () => {
-  test("Can parse hash with search parameters", () => {
-    const url =
-      "http://path/to/my/page.html#map=2.59/39.26/53.07/-24.1/60&foo=bar";
-
-    const expected = {
-      zoom: 2.59,
-      center: [53.07, 39.26],
-      bearing: -24.1,
-      pitch: 60,
-      qs: {
-        foo: "bar",
-      },
-    };
-
-    const parsed = parseMapPositionHash(url);
-
-    expect(parsed).toEqual(expected);
-  });
-
   test("Can parse hash without search parameters", () => {
     const url = "http://path/to/my/page.html#map=2.59/39.26/53.07/-24.1/60";
 
@@ -29,7 +10,6 @@ describe("parseMapPositionHash", () => {
       center: [53.07, 39.26],
       bearing: -24.1,
       pitch: 60,
-      qs: {},
     };
 
     const parsed = parseMapPositionHash(url);
