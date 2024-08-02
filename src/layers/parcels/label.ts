a0198c7948f0a43d628d3f24f4a03d201c85a630
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2024-08-01 17:16:23 -0700

    Parcels group layer w/ two choices (#412)
    * Revert "fix: :bug: Replaced constantly failing Parcels layer"
    
    This reverts commit c3d8355bde038473c4ea4cab2631a80909216b32.
    
    * refactor: added regrid
    
    * fix: :bug: Updated ArcGIS Online ID for parcels layer so it will work again.
    
    Maintainer of that service had to republish it
    which changed the ID.
    
    * refactor: :truck: Renamed parcels/index to parcels/watech
    
    * feat: :sparkles: User can now select which parcels layer they want to use

---
+++ src/layers/parcels/label.ts
@@ -0,0 +1,13 @@
+import TextSymbol from "@arcgis/core/symbols/TextSymbol";
+
+const outlineColor = "white";
+
+export const labelSymbol = new TextSymbol({
+  color: outlineColor,
+  haloColor: "black",
+  haloSize: 1,
+  font: {
+    size: 10,
+    weight: "normal",
+  },
+});
---
+++ src/layers/parcels/regrid.ts
@@ -0,0 +1,20 @@
+const arcGisOnlineId = "a2050b09baff493aa4ad7848ba2fac00";
+
+const [{ default: TileLayer }, { default: PortalItem }] = await Promise.all([
+  import("@arcgis/core/layers/TileLayer"),
+  import("@arcgis/core/portal/PortalItem"),
+]);
+
+/**
+ * Parcels layer
+ * @see {@link https://wsdot.maps.arcgis.com/home/item.html?id=9c7f4cd9a1354d8db79c471be957a0d2}
+ */
+export const parcelsLayer = new TileLayer({
+  id: "parcels",
+  // visible: false,
+  portalItem: new PortalItem({
+    id: arcGisOnlineId,
+  }),
+  listMode: "hide-children",
+  legendEnabled: false,
+});
