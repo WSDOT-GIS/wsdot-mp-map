f591312920ecb9596b21414e86de1ffc99d70c0e
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2024-08-05 16:52:00 -0700

    feat: :sparkles: Added page title to header

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,6 +5,8 @@ interface ImportMetaEnv {
   readonly VITE_WEBMAP_PORTAL_URL: string;
   readonly VITE_WEBMAP_PORTAL_ITEM_ID: string;
   readonly VITE_ZOOM_SCALE: `${number}`;
+  readonly VITE_TITLE: string;
+  readonly VITE_HEADER_TITLE: string;
   // more env variables...
 }
 
