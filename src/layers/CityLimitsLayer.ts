d86e3e970aa90b4c025e936acece5a56f8aa4fb5
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2024-08-13 17:24:50 -0700

    Cleanup (#424)
    * refactor: :coffin: Removed unused city query function
    
    * refactor: :coffin: Removed county boundaries query

--- src/layers/CityLimitsLayer.ts
+++ src/layers/CityLimitsLayer.ts
@@ -1,10 +1,7 @@
-import type { AttributesObject } from "../types";
-import type Point from "@arcgis/core/geometry/Point";
 import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
 import SimpleRenderer from "@arcgis/core/renderers/SimpleRenderer";
 import SimpleFillSymbol from "@arcgis/core/symbols/SimpleFillSymbol";
 import SimpleLineSymbol from "@arcgis/core/symbols/SimpleLineSymbol";
-import type FeatureLayerView from "@arcgis/core/views/layers/FeatureLayerView";
 
 const outFields = ["CityName", "LastUpdate"];
 
@@ -19,8 +16,6 @@ const renderer = new SimpleRenderer({
   }),
 });
 
-let cityLimitsLayerView: FeatureLayerView | undefined;
-
 /**
  * The city limits layer.
  */
@@ -32,58 +27,3 @@ export const cityLimitsLayer = new FeatureLayer({
   popupEnabled: false,
   renderer,
 });
-
-cityLimitsLayer.on("layerview-create", ({ layerView }) => {
-  cityLimitsLayerView = layerView as FeatureLayerView;
-});
-
-export default cityLimitsLayer;
-
-/**
- * Represents the attributes for city limits.
- */
-export interface CityLimitsAttributes extends AttributesObject {
-  /** The name of the city. */
-  CityName: string;
-
-  /** The timestamp of the last update. */
-  LastUpdate: number;
-
-  /** The unique identifier for the city limits object. */
-  OBJECTID: number | null;
-
-  /** Indicates if the city is a county seat. */
-  CountySeat: string | null;
-
-  /** The GNIS code of the city. */
-  CityGNISPlaceCode: number | null;
-
-  /** The long-form FIPS code of the county. */
-  CountyFIPSLongPlaceCode: string | null;
-
-  /** Indicates if the city is considered a major city. */
-  MajorCity: string | null;
-}
-
-/**
- * Queries the city limits based on the provided point.
- * @param point - The point to query the city limits for.
- * @returns - The city limits attributes for the provided point, or null if no city limits are found.
- */
-export async function queryCityLimits(point: Point) {
-  const results = await (cityLimitsLayerView ?? cityLimitsLayer).queryFeatures({
-    geometry: point,
-    spatialRelationship: "within",
-    outFields: outFields,
-  });
-
-  if (results.features.length > 1) {
-    console.warn("Multiple city limits found", results.features);
-  }
-
-  return (
-    results.features
-      .map(({ attributes }) => attributes as CityLimitsAttributes)
-      .at(0) ?? null
-  );
-}
