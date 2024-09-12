7f8f91117c0294ebfa47a34da1bb9483f47cabae
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2024-09-12 13:07:55 -0700

    refactor: :rotating_light: Replaced deprecated Calcite modal with dialog

--- src/components/disclaimer/index.ts
+++ src/components/disclaimer/index.ts
@@ -1,47 +1,38 @@
-import content from "./content.html?raw";
-
-/**
- * Creates the disclaimer modal
- * @returns The disclaimer modal
- */
-export function createModal() {
-  const modal = document.createElement("calcite-modal");
-  modal.setAttribute("aria-labelledby", "modal-title");
-
-  modal.innerHTML = content;
-  return modal;
-}
-
 /**
  * Sets up a link to open a modal when it is clicked.
  * @param link - The link that will open the modal when clicked.
- * @param modal - The modal that will be opened. If omitted, a new
+ * @param dialog - The modal that will be opened. If omitted, a new
  * one will be created and appended to the document.
  * @returns The link and the modal.
  */
 export function setupDisclaimerLink(
   link: HTMLElement,
-  modal?: HTMLCalciteModalElement,
+  dialog?: HTMLCalciteDialogElement,
 ) {
-  if (!modal) {
-    modal = createModal();
-    document.body.appendChild(modal);
+  if (!dialog) {
+    dialog =
+      document.body.querySelector<HTMLCalciteDialogElement>(
+        "#disclaimer-dialog",
+      ) ?? undefined;
+  }
+  if (!dialog) {
+    throw new Error("Could not find disclaimer dialog.");
   }
   if (/WSDOT-FOOTER/i.test(link.tagName)) {
     // Setup disclaimer modal
     link.addEventListener("disclaimer-link-clicked", () => {
-      modal.open = true;
+      dialog.open = true;
     });
   } else {
     // Setup disclaimer modal
     link.addEventListener("click", (e) => {
-      modal.open = true;
+      dialog.open = true;
       e.preventDefault();
     });
   }
 
   return {
-    modal,
+    modal: dialog,
     link,
   };
 }
