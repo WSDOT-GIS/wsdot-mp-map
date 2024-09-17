a0b496a4bee17f210d4e337c7221212f2ba1b2ee
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2024-09-17 10:23:43 -0700

    build: :hammer: Added script to create shield icon defs from SVG files

--- tools/copy-calcite-assets.ts
+++ tools/copy-calcite-assets.ts
@@ -2,13 +2,10 @@
  * Copies Calcite assets from node_modules into public/calcite/assets.
  * See: https://developers.arcgis.com/calcite-design-system/get-started/#load-the-assets
  */
-import {
-  interstateShield21,
-  stateRouteShield21,
-  usRouteShield21,
-} from "calcite-point-symbols";
-import { cp, mkdir, writeFile } from "fs/promises";
+import type { Dirent } from "fs";
+import { cp, mkdir, readFile, readdir, writeFile } from "fs/promises";
 import { dirname, join } from "path";
+import { parse as parseSvg } from "svgson";
 import { fileURLToPath } from "url";
 
 /** The root path of this project */
@@ -25,6 +22,83 @@ const sourceAssetsFolder = join(
   "calcite",
   "assets",
 );
+
+/**
+ * Converts all SVG files in `iconSourceFolder` to JSON files in
+ * `iconDestinationFolder` by extracting the d attribute from the
+ * single path element in each SVG file and writing it to a JSON file
+ * with the same name as the SVG file but with a .json extension.
+ * If the SVG file does not contain a single path element, it is
+ * skipped.
+ */
+async function createRouteShieldIconDefinitionJsonFiles() {
+  /**
+   * Converts a single SVG file to a JSON file by extracting the d
+   * attribute from the single path element in the SVG file and writing
+   * it to a JSON file with the same name as the SVG file but with a
+   * .json extension.
+   * @param svgPath - A `Dirent` object representing the SVG file to
+   *   convert.
+   */
+  async function convertSvgToJson(svgPath: Dirent) {
+    const svg = await readFile(join(iconSourceFolder, svgPath.name), {
+      encoding: "utf-8",
+    });
+    const svgAsObject = await parseSvg(svg);
+
+    // SVG is assumed to have only a single path element.
+    // All paths assumed to be at root level.
+    const paths = svgAsObject.children.filter((node) => node.name === "path");
+
+    let jsonText: string;
+    if (paths.length === 1) {
+      jsonText = JSON.stringify(paths[0].attributes.d);
+    } else {
+      jsonText = JSON.stringify(paths);
+    }
+
+    const outFilePath = join(
+      iconDestinationFolder,
+      svgPath.name.replace(".svg", ".json"),
+    );
+    await writeFile(outFilePath, jsonText);
+  }
+
+  console.log("Creating route shield icon definition json files...");
+
+  const iconSourceFolder = join(rootDirPath, "public", "highway-shields");
+
+  const iconDestinationFolder = join(
+    rootDirPath,
+    "public",
+    "calcite",
+    "assets",
+    "icon",
+  );
+
+  // Read the contents of the source directory.
+  const dirContents = await readdir(iconSourceFolder, {
+    withFileTypes: true,
+    recursive: false,
+    encoding: "utf-8",
+  });
+
+  // Filter to only SVG files.
+  const svgFilePaths = dirContents.filter(
+    (dirent) => dirent.isFile() && /^\w+\d+\.svg$/gi.test(dirent.name),
+  );
+
+  // Create the destination folder if it doesn't exist.
+  await mkdir(iconDestinationFolder, { recursive: true });
+
+  const svgPromises = svgFilePaths.map((svgFilePath) =>
+    convertSvgToJson(svgFilePath),
+  );
+
+  await Promise.all(svgPromises);
+}
+
+await createRouteShieldIconDefinitionJsonFiles();
 
 console.log(
   `Copying Calcite assets from ${sourceAssetsFolder} to ${destinationFolder}...`,
@@ -44,35 +118,3 @@ await cp(sourceAssetsFolder, destinationFolder, {
     return !src.endsWith("package.json") && !src.endsWith("README.md");
   },
 });
-
-function copyCalcitePointIcons() {
-  /**
-   * The folder that the icon files will be written to.
-   */
-  const iconsFolder = join(destinationFolder, "icon");
-
-  const iconNames = {
-    interstateShield21,
-    stateRouteShield21,
-    usRouteShield21,
-  };
-
-  const writeIconPromises: ReturnType<typeof writeFile>[] = [];
-
-  for (const [iconName, svgData] of Object.entries(iconNames)) {
-    const iconPath = join(iconsFolder, `${iconName.replace(/21$/, "16")}.json`);
-    const writePromise = writeFile(iconPath, JSON.stringify(svgData));
-    writeIconPromises.push(writePromise);
-    writePromise
-      .then(() => {
-        console.log(`Wrote ${iconPath}`);
-      })
-      .catch((error: unknown) => {
-        console.error(`Error writing ${iconPath}`, error);
-      });
-  }
-
-  return Promise.allSettled(writeIconPromises);
-}
-
-await copyCalcitePointIcons();
