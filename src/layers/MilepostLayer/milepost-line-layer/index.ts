f5008ac017ccb2e09dd1e449ab69fac5793069fc
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2024-11-25 14:35:17 -0800

    feat: :sparkles: Line layer now has same popup template as point layer

--- src/layers/MilepostLayer/milepost-line-layer/index.ts
+++ src/layers/MilepostLayer/milepost-line-layer/index.ts
@@ -1,8 +1,8 @@
-import { fields } from "..";
+import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
+import { createPopupTemplate, fields } from "..";
 import waExtent from "../../../WAExtent";
 import { objectIdFieldName } from "../../../elc/types";
 import MilepostOffsetLineRenderer from "./MilepostOffsetLineRenderer";
-import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
 
 /**
  * Creates a new feature layer that displays mileposts as lines.
@@ -10,29 +10,29 @@ import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
  * @returns A new feature layer that displays mileposts as lines.
  */
 export function createMilepostLineLayer(
-  spatialReference = waExtent.spatialReference,
+	spatialReference = waExtent.spatialReference,
 ) {
-  // Make a clone of the milepost point layer, as most of the properties
-  // will be the same aside from the geometry type and renderer.
-  const lineLayerProperties: __esri.FeatureLayerProperties = {
-    geometryType: "polyline",
-    title: "Near Mileposts",
-    fields,
-    objectIdField: objectIdFieldName,
-    id: "nearMileposts",
-    listMode: "hide",
-    fullExtent: waExtent,
-    spatialReference,
-    // Since there are no features at the beginning,
-    // need to add an empty array as the source.
-    renderer: MilepostOffsetLineRenderer,
-    source: [],
-    popupEnabled: true,
-    hasM: true,
-  };
+	// Make a clone of the milepost point layer, as most of the properties
+	// will be the same aside from the geometry type and renderer.
+	const lineLayerProperties: __esri.FeatureLayerProperties = {
+		geometryType: "polyline",
+		title: "Near Mileposts",
+		fields,
+		objectIdField: objectIdFieldName,
+		id: "nearMileposts",
+		listMode: "hide",
+		fullExtent: waExtent,
+		spatialReference,
+		// Since there are no features at the beginning,
+		// need to add an empty array as the source.
+		renderer: MilepostOffsetLineRenderer,
+		source: [],
+		popupEnabled: true,
+		hasM: true,
+	};
 
-  const lineLayer = new FeatureLayer(lineLayerProperties);
-  lineLayer.popupTemplate = lineLayer.createPopupTemplate();
+	const lineLayer = new FeatureLayer(lineLayerProperties);
+	lineLayer.popupTemplate = createPopupTemplate(lineLayer);
 
-  return lineLayer;
+	return lineLayer;
 }
