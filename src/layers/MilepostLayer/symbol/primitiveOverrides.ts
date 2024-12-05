b78ac67f687b9c3ec952b78c9a6fc8fcd235f975
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2024-12-05 11:54:30 -0800

    refactor: :recycle: refactor milepost CIM definitions

--- src/layers/MilepostLayer/symbol/primitiveOverrides.ts
+++ src/layers/MilepostLayer/symbol/primitiveOverrides.ts
@@ -1,28 +1,39 @@
+/**
+ * The expression info for the primitive override for the milepost label symbol.
+ * This will be used on the milepost line layer.
+ */
 const beginMilepostExpressionInfo = {
 	expression: "`${$feature.Route}\\n${$feature.SRMP}${$feature.Back}`",
 	type: "CIMExpressionInfo",
 	returnType: "String",
 } as const;
+
+/**
+ * The expression info for the primitive override for the end milepost label symbol.
+ * This will be used on the route segment line layer.
+ */
 const endMilepostExpressionInfo = {
 	...beginMilepostExpressionInfo,
 	expression: "`${$feature.Route}\\n${$feature.EndSrmp}${$feature.EndBack}`",
 } as const;
-const milepostLabelPrimitiveOverride: __esri.PrimitiveOverride = {
+
+/**
+ * The primitive override for the milepost label symbol.
+ * This will be used on the milepost line layer.
+ */
+export const milepostLabelPrimitiveOverride = {
 	primitiveName: "milepostLabel",
 	propertyName: "textString",
 	type: "CIMPrimitiveOverride",
 	valueExpressionInfo: beginMilepostExpressionInfo,
 } as const;
-const endMilepostLabelPrimitiveOverride: __esri.PrimitiveOverride = {
+
+/**
+ * The primitive override for the end milepost label symbol.
+ * This will be used on the route segment line layer.
+ */
+export const endMilepostLabelPrimitiveOverride = {
 	...milepostLabelPrimitiveOverride,
 	primitiveName: "endMilepostLabel",
 	valueExpressionInfo: endMilepostExpressionInfo,
-};
-
-export const primitiveOverrides: __esri.PrimitiveOverride[] = [
-	milepostLabelPrimitiveOverride,
-];
-export const routeSegmentPrimitiveOverrides = [
-	milepostLabelPrimitiveOverride,
-	endMilepostLabelPrimitiveOverride,
-];
+} as const;
