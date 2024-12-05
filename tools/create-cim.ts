f655572b37c09a50df34f51311754158cf27264c
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2024-12-04 17:41:28 -0800

    chore: :hammer: create-cim script now adds primitive overrides

--- tools/create-cim.ts
+++ tools/create-cim.ts
@@ -1,3 +1,8 @@
+/**
+ * This script was used to create the milepost marker symbol,
+ * after which it was further modified.
+ */
+
 import TextSymbol from "@arcgis/core/symbols/TextSymbol";
 import { convertToCIMSymbol } from "@arcgis/core/symbols/support/cimConversionUtils";
 import {
@@ -15,4 +20,34 @@ const cimSymbol = convertToCIMSymbol(
 	simpleSymbol as unknown as __esri.SimpleMarkerSymbol,
 );
 
-console.log(JSON.stringify(cimSymbol.toJSON()));
+cimSymbol.data.primitiveOverrides = [
+	{
+		primitiveName: "milepostLabel",
+		propertyName: "textString",
+		valueExpressionInfo: {
+			expression: "`${$feature.Route}\\n${$feature.SRMP}${$feature.Back}`",
+			type: "CIMExpressionInfo",
+			returnType: "String",
+		},
+	},
+];
+
+if (cimSymbol.data.symbol?.symbolLayers) {
+	let found = false;
+	for (const layer of cimSymbol.data.symbol.symbolLayers.filter(
+		(l) => l.type === "CIMVectorMarker",
+	)) {
+		for (const markerGraphic of layer.markerGraphics.filter(
+			(g) => g.symbol.type === "CIMTextSymbol",
+		)) {
+			markerGraphic.primitiveName = "milepostLabel";
+			found = true;
+			break;
+		}
+		if (found) {
+			break;
+		}
+	}
+}
+
+console.log(JSON.stringify(cimSymbol.toJSON(), null, 2));
