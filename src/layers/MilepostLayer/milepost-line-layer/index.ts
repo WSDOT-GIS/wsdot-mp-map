547ba8c0087da6f83dd6233b78bd0067aeed9421
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2025-01-16 11:17:07 -0800

    Revert "feat: removed label from line segment"
    This reverts commit a841625150fbe7b1e913edf39f0eaa0ba9b2569c.

--- src/layers/MilepostLayer/milepost-line-layer/index.ts
+++ src/layers/MilepostLayer/milepost-line-layer/index.ts
@@ -1,11 +1,39 @@
 import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
+import LabelClass from "@arcgis/core/layers/support/LabelClass";
 import ExpressionInfo from "@arcgis/core/popup/ExpressionInfo";
+import { TextSymbol } from "@arcgis/core/symbols";
 import { createPopupTemplate } from "..";
 import waExtent from "../../../WAExtent";
+import { highwaySignTextColor } from "../../../colors";
 import { objectIdFieldName } from "../../../elc/types";
 import { routeSegmentLabelExpressionInfo } from "../arcade";
 import { segmentFields as fields } from "../fields";
 import MilepostOffsetLineRenderer from "./MilepostOffsetLineRenderer";
+
+const lineSegmentLabelClass = new LabelClass({
+	where: "EndSrmp IS NOT NULL",
+	labelExpressionInfo: routeSegmentLabelExpressionInfo,
+	labelPlacement: "above-along",
+	labelPosition: "curved",
+	minScale: 144447.638572,
+	allowOverrun: true,
+	repeatLabel: false,
+	symbol: new TextSymbol({
+		color: highwaySignTextColor,
+		verticalAlignment: "baseline",
+		haloColor: "black",
+		haloSize: 2.5,
+		// // The background only shows up when zoomed out enough that the label overruns its polyline feature.
+		// backgroundColor: highwaySignBackgroundColor,
+		// borderLineColor: highwaySignTextColor,
+		// borderLineSize: 1,
+		font: {
+			family: "Arial",
+			size: 10,
+			weight: "bold",
+		},
+	}),
+});
 
 /**
  * Creates a new feature layer that displays mileposts as lines.
@@ -32,6 +60,7 @@ export function createMilepostLineLayer(
 		source: [],
 		popupEnabled: true,
 		hasM: true,
+		labelingInfo: [lineSegmentLabelClass],
 	};
 
 	const lineLayer = new FeatureLayer(lineLayerProperties);
