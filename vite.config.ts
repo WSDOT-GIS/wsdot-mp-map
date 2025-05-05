750267f63967b092c88545ef5f08721ae9d648a0
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2025-05-05 12:31:58 -0700

    build: :wrench: Vite config: Set all @arcgis & @esri deps to external

--- vite.config.ts
+++ vite.config.ts
@@ -1,140 +1,61 @@
 /// <reference types="vite/client" />
 /// <reference types="vitest" />
 import browserslistToEsbuild from "browserslist-to-esbuild";
-import { type BuildEnvironmentOptions, defineConfig } from "vite";
+import { defineConfig } from "vite";
 
-type ExternalOption = NonNullable<
-	BuildEnvironmentOptions["rollupOptions"]
->["external"];
-type ExternalFunction = Exclude<ExternalOption, string | RegExp | unknown[]>;
-
-/**
- * Matches l18n files with a language code in the form of `en_US` or `en_GB`, or `zh-Hans` or `zh-Hant`.
- */
-const languageRe = /^(?<lang>[a-z]{2})_(?<region>([A-Z]{2})|([a-zA-Z]{4}))/;
-
-/**
- * Determines if the module name is for a non-English language using
- * {@link languageRe}.
- * @param name - module name
- * @returns true if the module name is for a non-English language,
- * false otherwise.
- */
-function isNonEnglishLanguage(name: string) {
-	// If the filename matches the language pattern, check if it's not English.
-	const langMatch = languageRe.exec(name);
-	if (langMatch?.groups) {
-		const { lang, region } = langMatch.groups;
-		console.log(
-			// Log the filename and the language it matches.
-			`${name} is a language: ${lang}-${region}.`,
-		);
-		// If the language doesn't match English, add it to the delete list.
-		if (langMatch.groups.lang !== "en") {
-			console.log(`Excluding non-English language "${name}".`);
-			return true;
-		}
-	}
-	return false;
-}
-
-/**
- * Specifies which modules can be excluded.
- * @see {@link https://community.esri.com/t5/arcgis-javascript-maps-sdk-questions/building-in-vite-includes-unnecessary-files/m-p/1237759|"Building in vite includes unnecessary files" on _Esri Community_}
- */
-const external: ExternalFunction = (id, _parentId, _isResolved) => {
-	if (!/\bnode_modules([/\\])@arcgis\1core\b/i.test(id)) {
-		return false;
-	}
-
-	// /**
-	//  * A list of regular expressions. If an id
-	//  * matches any of these, it will be skipped.
-	//  */
-	// const toSkip = [
-	// 	/BingMaps/i,
-	// 	/GeoJSON/i,
-	// 	/GeoRSS/i,
-	// 	/integratedMeshLayer/i,
-	// 	/KML/i,
-	// 	// /KnowledgeGraph/i,
-	// 	// /OGCFeature/i,
-	// 	/OpenStreetMap/i,
-	// 	// /Oriented/i,
-	// 	/PointCloud/i,
-	// 	/RouteLayer/i,
-	// 	/sphere/i,
-	// 	// /StreamLayer/i,
-	// 	/Video/i,
-	// 	/Voxel/i,
-	// 	/WFS/i,
-	// 	/WMT?SLayer/i,
-	// ];
-	const shouldSkip = isNonEnglishLanguage(id);
-	// ||
-	// toSkip.some((re) => {
-	// 	const match = re.exec(id);
-	// 	if (!match) {
-	// 		return false;
-	// 	}
-
-	// 	return true;
-	// });
-
-	return shouldSkip;
-};
-
-export default defineConfig((env) => {
-	return {
-		appType: "spa",
-		base: "/data/tools/LocateMP",
-		resolve: {
-			extensions: [".ts", ".js", ".css", ".json"],
-		},
-		build: {
-			// This sets the target based on the `browserslist` file.
-			target: browserslistToEsbuild(),
-			rollupOptions: {
-				input: {
-					main: "./index.html",
-					"route-list": "./route-list.html",
-					webmap: "./webmap.html",
-				},
-				external,
+export default defineConfig((env) => ({
+	appType: "spa",
+	base: "/data/tools/LocateMP",
+	resolve: {
+		extensions: [".ts", ".js", ".css", ".json"],
+	},
+	build: {
+		// This sets the target based on the `browserslist` file.
+		target: browserslistToEsbuild(),
+		rollupOptions: {
+			input: {
+				main: "./index.html",
+				"route-list": "./route-list.html",
+				webmap: "./webmap.html",
 			},
-			sourcemap: true,
+			external: [
+				"@arcgis/core",
+				"@arcgis/map-components",
+				"@esri/calcite-components",
+			],
 		},
-		test: {
-			name: "LocateMP",
-			environment: "jsdom",
-			setupFiles: "./tests/mocks-setup.ts",
-			reporters: ["default", "junit"],
-			outputFile: {
-				junit: "./test-results/test-results.xml",
-			},
-			coverage: {
-				enabled: false,
-				extension: [
-					// '.js',
-					// '.cjs',
-					// '.mjs',
-					// ".jsx",
-					".ts",
-					".tsx",
-					// ".vue",
-					// ".svelte",
-				],
-				reporter: [
-					// Defaults
-					"text",
-					"html",
-					"clover",
-					"json",
-					// Added
-					"lcov",
-					"cobertura",
-				],
-			},
+		sourcemap: true,
+	},
+	test: {
+		name: "LocateMP",
+		environment: "jsdom",
+		setupFiles: "./tests/mocks-setup.ts",
+		reporters: ["default", "junit"],
+		outputFile: {
+			junit: "./test-results/test-results.xml",
 		},
-	};
-});
+		coverage: {
+			enabled: false,
+			extension: [
+				// '.js',
+				// '.cjs',
+				// '.mjs',
+				// ".jsx",
+				".ts",
+				".tsx",
+				// ".vue",
+				// ".svelte",
+			],
+			reporter: [
+				// Defaults
+				"text",
+				"html",
+				"clover",
+				"json",
+				// Added
+				"lcov",
+				"cobertura",
+			],
+		},
+	},
+}));
